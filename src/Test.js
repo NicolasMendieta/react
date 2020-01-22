6c749f34ef692090482d22646a190798646ed0ad
Esteban Dominguez <estebantalavera17@gmail.com>
2020-01-22 10:05:28 -0300

    empezando con reac

---
+++ src/Test.js
@@ -0,0 +1,21 @@
+import
+  React from 'react';
+import './App.css';
+
+function Test() {
+  // return (
+  //     React.createElement('div',{},
+  //         React.createElement('button',{},"Boton creado desde el dom")
+  //         )
+  // );
+
+
+
+  return (
+  <div>
+    <button>boton jsx</button>
+  </div>
+  );
+}
+
+export default Test;
